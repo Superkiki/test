f8788181d77aed4b74449bb424fe87ba4e3922c0
Superkiki <zegogo93@gmail.com>
2013-12-27 00:40:11 +0100

    first commit

---
+++ toto/Main.h
@@ -0,0 +1,18 @@
+/*
+ * Main.h
+ *
+ *  Created on: 26 déc. 2013
+ *      Author: hsellou
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+class Main {
+public:
+	Main();
+	virtual ~Main();
+
+};
+
+#endif /* MAIN_H_ */
